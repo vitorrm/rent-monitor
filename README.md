2dea5c5e74914c42e3ccc1a7fc0ae7008da38122
Vitor R Munhoz <vitor.rm@gmail.com>
2020-12-24 15:09:47 -0300

    Update action to run every hour

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Very specific stuff, you do not want to use that, but feel free to look
 
-'''
-npm start -- --email-user xxx --email-pass: xxx
-'''
+```
+npm start -- --email-user xxx --email-pass xxx
+```
--- send-email.js
+++ send-email.js
@@ -1,7 +1,14 @@
 const { EmailSender } = require('./lib/EmailSender');
 const { House } = require('./lib/House');
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
 
-const sender = new EmailSender();
+const sender = new EmailSender({
+    user: argv.emailUser,
+    pass: argv.emailPass
+});
+
 sender.sendHouseUpdateEmail({
     newHouses: [
             new House({ url: 'testA' }),
