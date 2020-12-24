83843059e664a61d204f7a57e543f0c7851f978d
Vitor R Munhoz <vitor.rm@gmail.com>
2020-12-24 15:21:09 -0300

    Update action to run every hour

--- send-email.js
+++ send-email.js
@@ -1,17 +1,22 @@
-const { EmailSender } = require('./lib/EmailSender');
-const { House } = require('./lib/House');
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
-const argv = yargs(hideBin(process.argv)).argv
+(async () => {
+    const { EmailSender } = require('./lib/EmailSender');
+    const { House } = require('./lib/House');
+    const yargs = require('yargs/yargs')
+    const { hideBin } = require('yargs/helpers')
+    const argv = yargs(hideBin(process.argv)).argv
 
-const sender = new EmailSender({
-    user: argv.emailUser,
-    pass: argv.emailPass
-});
+    const sender = new EmailSender({
+        user: argv.emailUser,
+        pass: argv.emailPass
+    });
 
-sender.sendHouseUpdateEmail({
-    newHouses: [
+    await sender.sendHouseUpdateEmail({
+        newHouses: [
             new House({ url: 'testA' }),
             new House({ url: 'testB' }),
-    ]
-})
+        ]
+    })
+
+    sender.close()
+
+})()
